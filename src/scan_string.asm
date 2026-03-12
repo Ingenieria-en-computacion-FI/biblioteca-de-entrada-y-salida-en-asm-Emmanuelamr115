257e9752a26c912b19297db88353bf4e80ca9dec
Emmanuel Motta Reyes <Emmanuelamr2004@outlook.com>
2026-03-11 22:26:28 -0600

    Update scan_string.asm

--- src/scan_string.asm
+++ src/scan_string.asm
@@ -18,6 +18,15 @@ scan_string:
     ; 2. guardar en buffer
     ; 3. agregar terminador 0
 
+    mov ecx, eax
+    mov edx, ebx
+
+    mov eax, 3
+    mov ebx, 0
+    int 0x80
+
+    mov byte [ecx + eax - 1], 0
+
     mov esp, ebp
     pop ebp
     ret
