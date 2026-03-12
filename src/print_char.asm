9895930907acf0f446d8738ec420a6445cdf53e6
Emmanuel Motta Reyes <Emmanuelamr2004@outlook.com>
2026-03-11 22:23:15 -0600

    Update print_char.asm

--- src/print_char.asm
+++ src/print_char.asm
@@ -1,3 +1,6 @@
+SECTION .bss
+char_buffer resb 1
+
 SECTION .text
 global print_char
 
@@ -18,6 +21,14 @@ print_char:
     ; 3. Imprimir 1 byte
 
     ; write(fd=1, buffer, 1)
+
+    mov [char_buffer], al
+
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, char_buffer
+    mov edx, 1
+    int 0x80
 
     mov esp, ebp
     pop ebp
