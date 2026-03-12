4577bf4d8422b5cc92de993ed7ef2479ad1e8f1a
Emmanuel Motta Reyes <Emmanuelamr2004@outlook.com>
2026-03-11 22:25:45 -0600

    Update print_string.asm

--- src/print_string.asm
+++ src/print_string.asm
@@ -17,6 +17,24 @@ print_string:
     ; 1. calcular longitud
     ; 2. syscall write
 
+    mov esi, eax
+    xor edx, edx
+
+.calcular:
+
+    cmp byte [esi + edx], 0
+    je .imprimir
+
+    inc edx
+    jmp .calcular
+
+.imprimir:
+
+    mov ecx, eax
+    mov eax, 4
+    mov ebx, 1
+    int 0x80
+
     mov esp, ebp
     pop ebp
     ret
